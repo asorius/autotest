f099cb6e1ebd0f5afd806e4613a6531dfef64efa
asorius <18assdd@gmail.com>
2019-03-15 08:56:17 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const getData = require('./dataGetter');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// sends back data
+app.post('/api', async function(req, res) {
+  const { url } = req.body;
+  const data = await getData(url);
+  res.send(data);
+});
+
+app.listen(5000, () => {
+  console.log('server is up on 4000');
+});
