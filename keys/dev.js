02e2b13b38a0c751e0bf7f5f05e9d0751c50f8f0
asorius <18assdd@gmail.com>
2019-04-30 12:54:32 +0100

    trying to fix keys

--- keys/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://admin:adminas@cluster0-zcd64.mongodb.net/carlists?retryWrites=true',
-  googleAPI: 'AIzaSyDL2iDSF4s4uk1qPVCCF3ESBTZ4KnlBzdo',
-  postAPI: 'XbclxaaSKcqbL0T6Rx4A9NgjdG0kTv0L'
-};
--- keys/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  googleAPI: process.env.GOOGLE_API,
-  postAPI: process.env.POST_API
-};
