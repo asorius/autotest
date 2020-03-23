0a1c13cfb37ea5dee7facb21b72bb9c1ef619dd8
asorius <18assdd@gmail.com>
2020-03-23 20:08:18 +0000

    update

--- server.js
+++ server.js
@@ -17,52 +17,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// sends back data
-// app.post('/api', async function(req, res) {
-//   try {
-//     const { url } = req.body;
-//     const dataFromAutotrader = await getData(url);
-//     const mainData = {
-//       title: dataFromAutotrader.advert.title,
-//       sellerDescription: dataFromAutotrader.advert.description,
-//       images: dataFromAutotrader.advert.imageUrls,
-//       price: dataFromAutotrader.advert.price,
-//       partEx: dataFromAutotrader.advert.isPartExAvailable,
-//       averageMpg: dataFromAutotrader.pageData.tracking['average_mpg'],
-//       acceleration: dataFromAutotrader.pageData.tracking.acceleration,
-//       sellerInfo: {
-//         name: dataFromAutotrader.seller.name,
-//         trader: dataFromAutotrader.seller.isTradeSeller,
-//         phone1: dataFromAutotrader.seller.primaryContactNumber,
-//         phone2: dataFromAutotrader.seller.secondaryContactNumber,
-//         gmapLink: dataFromAutotrader.seller.locationMapLink
-//       },
-//       technical: { ...dataFromAutotrader.techSpecs }
-//     };
-//     const { vrm } = dataFromAutotrader.vehicle;
-//     const motData = await getMot(vrm);
-//     res.send({
-//       _id: Math.random(),
-//       mainData,
-//       ...dataFromAutotrader.vehicle,
-//       ...motData
-//     });
-//   } catch (e) {
-//     res.status(404).send({ error: e });
-//   }
-// });
-// app.post('/ddd', async function(req, res) {
-//   try {
-//     const { url } = req.body;
-//     const dataFromAutotrader = await getData(url);
 
-//     res.send({
-//       ...dataFromAutotrader
-//     });
-//   } catch (e) {
-//     res.status(404).send({ error: e });
-//   }
-// });
 app.post('/api/postcode', async (req, res) => {
   const { postcode } = req.body;
   if (postcode.length < 5 || postcode.length > 7) {
